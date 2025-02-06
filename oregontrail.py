d0c7d7fcc934875a2fea9ba34a1418c000e433c5
mr-hoffman <hoffmant@hilbertk12.org>
2025-02-06 12:03:49 -0600

    Create oregontrail.py

---
+++ oregontrail.py
@@ -0,0 +1,53 @@
+# 3.05 Oregon Trail
+
+day = 0
+miles_to_go = 2000
+health = 5
+food = 500
+commands = ["help", "travel", "quit", "hunt", "rest", "status"]
+
+############## helper functions ##############
+def add_days(min, max):
+  pass
+
+def update_health(val):
+  pass
+
+def update_food(val):
+  pass
+
+def update_miles(val):
+  pass
+
+############## command functions ##############
+def hunt():
+  pass
+
+def travel():
+  pass
+
+def rest():
+  pass
+
+# help() is a built-in Python function, so we need a different name for ours
+def player_help():
+  pass
+
+def status():
+  pass
+
+############## game loop ##############
+
+print("instructions go here")
+
+while day <= 305 and health > 0 and food > 0 and miles_to_go > 0 and quit == False:
+  pass
+
+############## check for win or loss conditions ##############
+
+# outside of the while loop, check to see why it ended; 
+# eg, if health reached zero, the player died, if miles_to_go reached zero, they won
+
+
+
+  
